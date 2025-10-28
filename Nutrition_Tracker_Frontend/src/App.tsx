89662168c3d01fdfdb6de2047ff0d314e9362342
MxnlodyZ <wattanachai.work@hotmail.com>
2025-10-29 05:27:43 +0700

    fix(Router) : add login page router and toastify

--- Nutrition_Tracker_Frontend/src/App.tsx
+++ Nutrition_Tracker_Frontend/src/App.tsx
@@ -1,21 +1,26 @@
 import { Routes, Route } from "react-router-dom";
-//import LoginPage from './pages/Login_Page'
+import SignInPage from './pages/SignIn_Page'
 import RegisterPage from './pages/Register_page'
 import NutritionMainPage from "./pages/Nutrition_Main";
+import ProtectedRoute from "./components/ProtectedRoutes";
 import "./style/App.css";
-
-
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 export default function App() {
   return (
-    <Routes>
-            {/* 
+    <>
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-      </Routes> 
-      */}
-      <Route path="/register" element={<RegisterPage />} />
-      <Route path="/nutritionTrack" element={<NutritionMainPage />} />
-    </Routes>
+        <Route path="/" element={<SignInPage />} />
+        <Route path="/login" element={<SignInPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/nutritionTrack"
+          element={
+            <ProtectedRoute>
+              <NutritionMainPage />
+            </ProtectedRoute>} />
+      </Routes>
+      <ToastContainer position="top-right" autoClose={3000} theme="colored" />
+    </>
 
 
   );
