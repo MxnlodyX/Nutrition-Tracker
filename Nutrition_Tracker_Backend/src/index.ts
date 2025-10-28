b7d7edd8deaa50dcef287e3c3bc9e1211ad1e9e0
MxnlodyZ <wattanachai.work@hotmail.com>
2025-10-29 05:27:24 +0700

    fix(routes): add cors

--- Nutrition_Tracker_Backend/src/index.ts
+++ Nutrition_Tracker_Backend/src/index.ts
@@ -5,14 +5,19 @@ import registrationRoutes from './routes/registrationRoute'
 import authRoutes from './routes/authRotues'
 import nutritionRotue from './routes/nutritionRoute'
 import { setupSwagger } from "./swagger";
+import cors from "cors";
 
 const app = express()
 dotenv.config()
 
 app.use(express.json())
-app.use('/api',dbtestRoutes)
-app.use('/api',registrationRoutes)
-app.use('/api/auth/',authRoutes)
+app.use(cors({
+  origin: "http://localhost:5173", // พอร์ตของ React/Vite
+  credentials: true, // ถ้าจะส่ง cookie/token ข้ามโดเมน
+}));
+app.use('/api', dbtestRoutes)
+app.use('/api', registrationRoutes)
+app.use('/api/auth/', authRoutes)
 app.use("/api/nutrition", nutritionRotue);
 
 setupSwagger(app);
