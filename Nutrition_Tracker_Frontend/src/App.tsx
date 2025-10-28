5d14519ca2c2f6494e21d0e12c4eac7be1b8fbf9
MxnlodyZ <wattanachai.work@hotmail.com>
2025-10-29 01:54:20 +0700

    fix(AppRouter) : Something is bruh remove default path waiting for new page

--- Nutrition_Tracker_Frontend/src/App.tsx
+++ Nutrition_Tracker_Frontend/src/App.tsx
@@ -1,16 +1,21 @@
 import { Routes, Route } from "react-router-dom";
-import LoginPage from './pages/Login_Page'
+//import LoginPage from './pages/Login_Page'
 import RegisterPage from './pages/Register_page'
-
+import NutritionMainPage from "./pages/Nutrition_Main";
 import "./style/App.css";
- 
+
 
 export default function App() {
   return (
+    <Routes>
+            {/* 
       <Routes>
         <Route path="/" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage/>}/>
-      </Routes>
+      </Routes> 
+      */}
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/nutritionTrack" element={<NutritionMainPage />} />
+    </Routes>
 
 
   );
